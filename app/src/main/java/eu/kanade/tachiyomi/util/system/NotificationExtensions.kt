c929d057e3a2c607eea816147ace64c576644fcc
Rojikku <Rojikku@users.noreply.github.com>
2026-07-05 03:23:50 -0400

    Implement accent colors

--- app/src/main/java/eu/kanade/tachiyomi/util/system/NotificationExtensions.kt
+++ app/src/main/java/eu/kanade/tachiyomi/util/system/NotificationExtensions.kt
@@ -66,7 +66,7 @@ fun Context.notificationBuilder(
     block: (NotificationCompat.Builder.() -> Unit)? = null,
 ): NotificationCompat.Builder {
     val builder = NotificationCompat.Builder(this, channelId)
-        .setColor(getColor(R.color.accent_blue))
+        .setColor(getColor(R.color.accent_color))
     if (block != null) {
         builder.block()
     }
